308c83783de7cad0e24a51b2100566c9d04f2f8e
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2021-05-15 16:09:56 +0200

    Fix minor typos

--- public_html/js/huischborrel.js
+++ public_html/js/huischborrel.js
@@ -52,8 +52,8 @@ setInterval(function(){
 
 var instemming = false;
 var instemmingDatum = "Maandag 11 Januari";
-var binnenkortInstemming = `Ja! Op <u> ${instemmingDatum} </u> is er een instemming. Lijkt het jou leuk om in een actief huis te komen wonen? Stuur dan een mailtje naar <a href="mailto:instemming@bolkhuis.ch"> instemming@bolkhuis.ch</a> en vertel daarin waarom jij de volgende actieve Huischbewoner gaat worden en waar je zoal van houdt`
-var geenInstemming = 'Nee, helaas nog niet :( op het moment hebben wij geen instemming op de planning staan. Maar als jij denkt "ik pas in dit leuke huis" stuur dan alvast een mailtje naar <a href="mailto:interesselijst@bolkhuis.ch">interesselijst@bolkhuis.ch</a> en danhouden we je op de hoogte wanneer er weer een instemming komt!'
+var binnenkortInstemming = `Ja! Op <u> ${instemmingDatum} </u> is er een instemming. Lijkt het jou leuk om in een actief huis te komen wonen? Stuur dan een mailtje naar <a href="mailto:instemming@bolkhuis.ch"> instemming@bolkhuis.ch</a> en vertel daarin waarom jij de volgende actieve Huischbewoner gaat worden en waar je zoal van houdt!`
+var geenInstemming = 'Nee, helaas nog niet :( op het moment hebben wij geen instemming op de planning staan. Maar als jij denkt "ik pas in dit leuke huis" stuur dan alvast een mailtje naar <a href="mailto:interesselijst@bolkhuis.ch">interesselijst@bolkhuis.ch</a> en dan houden we je op de hoogte wanneer er weer een instemming komt!'
 
 if(instemming){
     document.getElementById('instemmingsTekst').innerHTML = binnenkortInstemming;
