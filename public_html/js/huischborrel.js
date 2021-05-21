57ef210032d97a16e7a69b785b7a5a32ccacd88c
Aron Hoogeveen <aron.hoogeveen@gmail.com>
2021-05-21 08:45:55 +0200

    Add upcoming instemming

--- public_html/js/huischborrel.js
+++ public_html/js/huischborrel.js
@@ -50,8 +50,8 @@ setInterval(function(){
     getThirdThursdayOfTheMonth();
 }, 1000);
 
-var instemming = false;
-var instemmingDatum = "Maandag 11 Januari";
+var instemming = true;
+var instemmingDatum = "Maandag 31 mei";
 var binnenkortInstemming = `Ja! Op <u> ${instemmingDatum} </u> is er een instemming. Lijkt het jou leuk om in een actief huis te komen wonen? Stuur dan een mailtje naar <a href="mailto:instemming@bolkhuis.ch"> instemming@bolkhuis.ch</a> en vertel daarin waarom jij de volgende actieve Huischbewoner gaat worden en waar je zoal van houdt!`
 var geenInstemming = 'Nee, helaas nog niet :( op het moment hebben wij geen instemming op de planning staan. Maar als jij denkt "ik pas in dit leuke huis" stuur dan alvast een mailtje naar <a href="mailto:interesselijst@bolkhuis.ch">interesselijst@bolkhuis.ch</a> en dan houden we je op de hoogte wanneer er weer een instemming komt!'
 
